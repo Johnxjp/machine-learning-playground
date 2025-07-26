49aabe41185cd17dd8ed3374f6cf7f3d34031418
jxp <16599457+Johnxjp@users.noreply.github.com>
2025-07-27 00:11:54 +0100

    Fixed resnet'

--- resnet/model.py
+++ resnet/model.py
@@ -27,16 +27,16 @@ class ResidualCNNLayer(nn.Module):
 
     def __init__(
         self,
-        channels: int,
+        in_channels: int,
+        out_channels: int,
         kernel: int,
         layers: int,
-        downsample: bool = False,
+        stride: int,
         shortcut_method: str = "projection",
         batch_normalise: bool = True,
     ):
         """
-        in_channel : Number of input channels
-        out_channels : Number of output channels
+        channels : Number of input channels
         kernel : Kernel size for the convolution
         layers : Number of convolution layers to stack
         downsample : Reduce the feature size map by half applying stride of 2
@@ -50,57 +50,57 @@ class ResidualCNNLayer(nn.Module):
         self.layers = []
 
         # First layer
-        in_channels = channels
-        channels = channels * 2 if downsample else channels
         self.layers.append(
             nn.Conv2d(
                 in_channels,
-                channels,
+                out_channels,
                 kernel_size=kernel,
-                stride=2 if downsample else 1,
+                stride=stride,
                 bias=False,
                 padding=1,
             )
         )
         if batch_normalise:
-            self.layers.append(nn.BatchNorm2d(channels))
+            self.layers.append(nn.BatchNorm2d(out_channels))
 
         for i in range(1, layers):
             if i < layers - 1:
                 self.layers.append(
                     nn.Conv2d(
-                        channels,
-                        channels,
+                        out_channels,
+                        out_channels,
                         kernel_size=kernel,
                         bias=False,
                         padding=1,
                     )
                 )
                 if batch_normalise:
-                    self.layers.append(nn.BatchNorm2d(channels))
+                    self.layers.append(nn.BatchNorm2d(out_channels))
                 self.layers.append(nn.ReLU())
             else:
                 # Don't add ReLU to final layer. This happens after residual is added
                 self.layers.append(
                     nn.Conv2d(
-                        channels,
-                        channels,
+                        out_channels,
+                        out_channels,
                         kernel_size=kernel,
                         padding=1,
                         bias=False,
                     )
                 )
                 if batch_normalise:
-                    self.layers.append(nn.BatchNorm2d(channels))
+                    self.layers.append(nn.BatchNorm2d(out_channels))
 
         self.layers = nn.ModuleList(self.layers)
         self.residual_projection = nn.Identity()
-        if downsample:
+        if out_channels > in_channels:
             if shortcut_method == "projection":
                 # Reduce feature map size of input
-                self.residual_projection = nn.Conv2d(in_channels, channels, kernel_size=1, stride=2)
+                self.residual_projection = nn.Conv2d(
+                    in_channels, out_channels, kernel_size=1, stride=2
+                )
             else:
-                self.residual_projection = ShortcutPadLayer(channels)
+                self.residual_projection = ShortcutPadLayer(out_channels)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         residual = self.residual_projection(x)
@@ -123,19 +123,18 @@ class Resnet(nn.Module):
         super().__init__()
         self.image_size = image_size
         self.first_conv = nn.Conv2d(
-            in_channels, out_channels[0], kernel_size=input_kernel_size, stride=2, padding=1
+            in_channels, out_channels[0], kernel_size=input_kernel_size, stride=1, padding=1
         )
         self.residual_blocks = []
-        for i, c in enumerate(out_channels):
-            for b in range(blocks):
-                downsample = False
-                if b == 0 and i != 0:
-                    # Only downsample on the first block when channel size changes
-                    downsample = True
-
+        in_channels = out_channels[0]
+        for c in out_channels:
+            for _ in range(blocks):
+                stride = 2 if c > in_channels else 1
                 self.residual_blocks.append(
-                    ResidualCNNLayer(c, kernel=3, layers=2, downsample=downsample)
+                    ResidualCNNLayer(in_channels, c, kernel=3, layers=2, stride=stride)
                 )
+                in_channels = c
+
         self.residual_blocks = nn.ModuleList(self.residual_blocks)
         self.global_pool = nn.AdaptiveAvgPool2d((1, 1))
         self.out = nn.LazyLinear(n_classes)
@@ -144,6 +143,7 @@ class Resnet(nn.Module):
         x = self.first_conv(x)
         for layer in self.residual_blocks:
             x = layer(x)
-        
+
         x = self.global_pool(x)
+        x = x.view(x.size(0), -1)
         return self.out(x)
